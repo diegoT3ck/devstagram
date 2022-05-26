89bbb2ac94b5c04bb0a567418d7ad2569e710d73
Diego Rubio <diego.rubio@spr.gob.mx>
2022-05-26 10:47:43 -0500

    Segundo commit del curso Laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,18 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Devstagram - @yield('titulo')</title>
-    <link rel="stylesheet" src="{{asset('css/app.css')}}">
+
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <script src="{{asset('js/app.js')}}" defer></script>
 </head>
-<body class="bg-gray-500">
-{{--     <nav>
-        <a href="/">Principal</a>
-        <a href="/nosotros">Nosotros</a>
-        <a href="/tienda">Tienda</a>
-    </nav>   --}}
+<body class="bg-gray-100">
     <header class="p-5 border-b bg-white shadow">
-        <h1 class="text-3xl font-black">Devstagram</h1>
+        <div class="container mx-auto flex justify-between items-center">
+            <h1 class="text-3xl font-black">
+                Devstagram
+            </h1>
+            <nav>
+                <a href="#">Login</a>
+                <a href="#">Crear Cuenta</a>
+            </nav>
+
+        </div>
     </header>
+        {{-- <h1 class="text-2xl" >@yield('titullo')</h1><hr> --}}
     @yield('contenido')
     
 
---
+++ resources/views/pagina/nosotros.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('titulo')
+    Nosotros
+@endsection
+
+@section('contenido')
+    Este es el contenido sobre nosotross
+@endsection
